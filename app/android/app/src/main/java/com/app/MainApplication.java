a7c0fa2ec41b79320d4ce98206e4a48efb3f185c
philipp <schmidphilipp1995@web.de>
2018-10-21 01:53:47 +0200

    map init

--- app/android/app/src/main/java/com/app/MainApplication.java
+++ app/android/app/src/main/java/com/app/MainApplication.java
@@ -10,7 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.BV.LinearGradient.LinearGradientPackage; // <--- LinearGradient!
-
+import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new NfcManagerPackage(),
+             new MapsPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage() // <---- LinearGradient!
 
--- app/stacks/mapScreen/index.js
+++ app/stacks/mapScreen/index.js
@@ -1,20 +1,38 @@
-
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
-
-
+import MapView from "react-native-maps";
 class mapScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  };
-    }
-    render() {
-        return (
-          <View>
-            <Text>Hi            </Text>
-          </View>             
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: 48.1918371,
+            longitude: 11.6480125,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121
+          }}
+        />
+      </View>
+    );
+  }
 }
 
 export default mapScreen;
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
+});
