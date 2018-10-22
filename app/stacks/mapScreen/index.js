231bfe49ebac6c019a9f3478b425f784b1ef0da8
philipp <schmidphilipp1995@web.de>
2018-10-22 19:58:35 +0200

    finale ende ciao

--- app/stacks/mapScreen/index.js
+++ app/stacks/mapScreen/index.js
@@ -1,49 +1,49 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View, Image } from "react-native";
 import MapView from "react-native-maps";
 import { Marker } from "react-native-maps";
 class mapScreen extends Component {
   constructor(props) {
+    fetch(
+      "http://18.223.252.8:3000/location/getlocations/bygeolocation?lat=48.1518075&lon=11.5701609&range=0.21"
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ markers: data });
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
     super(props);
     this.state = {
       event: this.props.navigation.state.params.event,
       region: {
-        latitude: 48.1849552,
-        longitude: 11.6155324,
+        latitude: 48.1918359,
+        longitude: 11.6465513,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421
       },
-      markers: [
-        {
-          markerPos: {
-            latitude: 48.1849552,
-            longitude: 11.6155324
-          },
-          title: "Comfort Hotel am Medienpark"
-        }
-      ]
+      markers: [],
+      starter: {
+        latitude: 48.1918359,
+        longitude: 11.6465513
+      }
     };
   }
 
   onMapLayout = () => {
-    this.setState({ isMapReady: true });
+    setTimeout(() => {
+      this.setState({ isMapReady: true });
+    }, 100);
   };
 
   render() {
     return (
       <MapView
-        onPress={e => console.log(e.nativeEvent)}
         style={styles.map}
-        provider="google"
-        mapType="standard"
-        showsScale
-        showsCompass
-        showsPointsOfInterest
-        showsBuildings
-        showUserLocation
-        followUserLocation
-        loadingEnabled
-        showsMyLocationButton
+        showsUserLocation={true}
+        zoomEnabled={true}
         region={this.state.region}
         onLayout={this.onMapLayout}
       >
@@ -56,6 +56,10 @@ class mapScreen extends Component {
               description={marker.description}
             />
           ))}
+        <Marker
+          coordinate={this.state.starter}
+          image={require("../../src/dot.png")}
+        />
       </MapView>
     );
   }
